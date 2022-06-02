58a680dd14048228127aead3aab91020e514805d
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-06-02 10:59:42 -0500

    fase 3

--- src/test/java/Fase3/AirportTest.java
+++ src/test/java/Fase3/AirportTest.java
@@ -12,7 +12,7 @@ public class AirportTest {
     @Nested
     class EconomyFlightTest {
 
-        private EconomyFlight economyFlight;
+        private Flight economyFlight;
 
         @BeforeEach
         void setUp() {
@@ -25,11 +25,11 @@ public class AirportTest {
 
             assertEquals("1", economyFlight.getId());
             assertEquals(true, economyFlight.addPassenger(jessica));
-            assertEquals(1, economyFlight.getPassengers().size());
-            assertEquals("Jessica", economyFlight.getPassengers().get(0).getName());
+            assertEquals(1, economyFlight.getPassengersList().size());
+            assertEquals("Jessica", economyFlight.getPassengersList().get(0).getName());
 
             assertEquals(true, economyFlight.removePassenger(jessica));
-            assertEquals(0, economyFlight.getPassengers().size());
+            assertEquals(0, economyFlight.getPassengersList().size());
         }
 
         @Test
@@ -38,11 +38,11 @@ public class AirportTest {
 
             assertEquals("1", economyFlight.getId());
             assertEquals(true, economyFlight.addPassenger(cesar));
-            assertEquals(1, economyFlight.getPassengers().size());
-            assertEquals("Cesar", economyFlight.getPassengers().get(0).getName());
+            assertEquals(1, economyFlight.getPassengersList().size());
+            assertEquals("Cesar", economyFlight.getPassengersList().get(0).getName());
 
             assertEquals(false, economyFlight.removePassenger(cesar));
-            assertEquals(1, economyFlight.getPassengers().size());
+            assertEquals(1, economyFlight.getPassengersList().size());
         }
 
     }
@@ -50,7 +50,7 @@ public class AirportTest {
     @DisplayName("Dado que hay un vuelo negocios")
     @Nested
     class BusinessFlightTest {
-        private BusinessFlight businessFlight;
+        private Flight businessFlight;
 
         @BeforeEach
         void setUp() {
@@ -62,9 +62,9 @@ public class AirportTest {
             Passenger jessica = new Passenger("Jessica", false);
 
             assertEquals(false, businessFlight.addPassenger(jessica));
-            assertEquals(0, businessFlight.getPassengers().size());
+            assertEquals(0, businessFlight.getPassengersList().size());
             assertEquals(false, businessFlight.removePassenger(jessica));
-            assertEquals(0, businessFlight.getPassengers().size());
+            assertEquals(0, businessFlight.getPassengersList().size());
 
         }
 
@@ -73,9 +73,9 @@ public class AirportTest {
             Passenger cesar = new Passenger("Cesar", true);
 
             assertEquals(true, businessFlight.addPassenger(cesar));
-            assertEquals(1, businessFlight.getPassengers().size());
+            assertEquals(1, businessFlight.getPassengersList().size());
             assertEquals(false, businessFlight.removePassenger(cesar));
-            assertEquals(1, businessFlight.getPassengers().size());
+            assertEquals(1, businessFlight.getPassengersList().size());
 
         }
 
